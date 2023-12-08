7d2ba6463431b1ed594d124626cd1f063731e7ee
timdean27 <timdean27@yahoo.com>
2023-12-08 08:07:37 -0500

    user can upload photo and store in firebase, storage right now is not authenticated, need to creat date strucutre in firebase and have photosautheticated to specific users

--- you_map_2.0_front/you_map_2.0/src/components/MarkerPhotoPage.js
+++ you_map_2.0_front/you_map_2.0/src/components/MarkerPhotoPage.js
@@ -1,9 +1,45 @@
-import React from 'react'
+// MarkerPhotoPage.js
+import React, { useState } from 'react';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { storage } from '../firebase';
 
 const MarkerPhotoPage = () => {
-  return (
-    <div>MarkerPhotoPage</div>
-  )
-}
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [downloadURL, setDownloadURL] = useState(null);
 
-export default MarkerPhotoPage
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const uploadPhoto = async () => {
+    if (!selectedFile) {
+      console.error('No file selected');
+      return;
+    }
+
+    const storageRef = ref(storage, 'photos/' + selectedFile.name);
+    await uploadBytes(storageRef, selectedFile);
+    const url = await getDownloadURL(storageRef);
+
+    console.log('Photo uploaded:', selectedFile.name);
+    setDownloadURL(url);
+  };
+
+  return (
+    <div>
+      <h2>Marker Photo Page</h2>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={uploadPhoto}>Upload Photo</button>
+
+      {downloadURL && (
+        <div>
+          <p>Uploaded Photo:</p>
+          <img src={downloadURL} alt="Uploaded" style={{ maxWidth: '100%' }} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MarkerPhotoPage;
